21df1dfa0fd7bc1a506e7ad0eda92470944b6071
samdouble <samleafs@protonmail.com>
2023-08-21 22:38:57 -0400

    Linting

--- src/components/activities/ActivityPane.tsx
+++ src/components/activities/ActivityPane.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import RBForm from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
