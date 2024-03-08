98d3f9a6804927b0d3da918d5ee67c44b55f1751
samdouble <samleafs@protonmail.com>
2024-03-07 22:37:00 -0500

    chore: use configureStore instead of createStore

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,8 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { createLogger } from 'redux-logger';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import reducers from 'services/reducers';
+// import { api } from 'services/tasks/newEndpoints';
 
 let middlewares;
 if (process.env.NODE_ENV === 'production') {
@@ -12,7 +15,21 @@ if (process.env.NODE_ENV === 'production') {
   middlewares = [thunk, promise, logger];
 }
 
-const store = createStore(reducers, applyMiddleware(...middlewares));
+const store = configureStore({
+  reducer: reducers,
+  /*reducer: {
+    ...reducers,
+    // [api.reducerPath]: api.reducer,
+  },*/
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: getDefaultMiddleware => getDefaultMiddleware()
+      .concat(...middlewares)
+      //.concat(api.middleware),
+});
+
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+// setupListeners(store.dispatch);
 
 export default store;
 
