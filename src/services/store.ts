ca8b7b427deee60d4a29c9089b4cfed7db22b3ec
samdouble <samleafs@protonmail.com>
2024-03-07 22:37:16 -0500

    chore: use configureStore instead of createStore

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,4 +1,3 @@
-import { applyMiddleware, createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { createLogger } from 'redux-logger';
