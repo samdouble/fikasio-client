83a4898df2776aa7fa53041e51afcb2286308175
samdouble <samleafs@protonmail.com>
2024-12-04 20:32:34 -0500

    fix: fixed weird bug with editing an item in the ItemsList
    .

--- src/components/entities/items/ItemsList.tsx
+++ src/components/entities/items/ItemsList.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect, useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { useTranslation } from 'react-i18next';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { AutosaveTextarea, Checkbox, Table } from '@fikasio/react-ui-components';
-import DropdownToggle from 'components/UI/DropdownToggle';
-import { useAddItemMutation, useUpdateFieldValueForItemMutation, useDeleteItemMutation } from 'services/items/api';
-import links from 'utils/links';
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import { Checkbox } from '@fikasio/react-ui-components';
+import ItemRow from './ItemRow';
 import './ItemsList.scss';
 
 const ItemsList = ({
@@ -14,183 +9,92 @@ const ItemsList = ({
   items,
   onAddItem,
   onItemSelect,
+  onSelectAllItems,
   selectedItems,
 }) => {
-  const navigate = useNavigate();
-  const { t } = useTranslation();
-  const [rows, setRows] = useState(items);
+  const allItemsAreChecked = !!items?.length
+    && (items?.length === selectedItems.length);
 
-  const [createItem] = useAddItemMutation();
-  const [updateFieldValueForItem] = useUpdateFieldValueForItemMutation();
-  const [deleteItem] = useDeleteItemMutation();
-
-  useEffect(() => {
-    setRows([
-      ...((!items || !items.length) ? [{}] : []),
-      ...(items || []),
-    ]);
-  }, [items]);
-
-  const handleKeyDown = (e, field) => {
-    if (e.key === 'Enter' && field.type !== 'TEXT') {
-      onAddItem({})
-        .then(resultItem => {
-          if (resultItem) {
-            // const elementWithId = document.getElementById(resultItem.id!);
-            // TODO Make this work with refs
-            // (elementWithId as HTMLElement).focus();
+  const addItem = async item => {
+    onAddItem(item)
+      .then(resultItem => {
+        if (resultItem) {
+          const elementsWithClassname = document.getElementsByClassName(resultItem.id);
+          const nbElementsWithClassname = elementsWithClassname.length;
+          if (nbElementsWithClassname) {
+            (elementsWithClassname[nbElementsWithClassname - 1] as HTMLElement).focus();
           }
-        });
-      e.preventDefault();
-    }
-  };
-
-  const handleKeyUp = (e, updatedItem) => {
-    if (!updatedItem && e.target.textContent !== '') {
-      createItem({
-        entityId: entity.id,
+        }
       });
-    }
   };
 
   return (
     <Table
-      columns={[
-        {
-          type: 'checkbox',
-          isChecked: row => selectedItems.find(selectedItem => selectedItem.id === row.id),
-          onClick: row => onItemSelect(row),
-        },
-        {
-          type: 'numbering',
-        },
-        ...entity.fields
-          .map(field => {
-            return {
-              name: field.name,
-              render: row => {
-                const defaultValue = row.values?.find(val => val.fieldId === field.id)?.value;
-                if (['NUMBER', 'STRING', 'TEXT'].includes(field.type)) {
-                  return (
-                    <AutosaveTextarea
-                      className="itemRow_field_editable"
-                      defaultValue={defaultValue || ''}
-                      onKeyDown={e => handleKeyDown(e, field)}
-                      onKeyUp={e => handleKeyUp(e, row)}
-                      onSave={async value => {
-                        if (row.id) {
-                          updateFieldValueForItem({
-                            entityId: entity.id,
-                            itemId: row.id,
-                            fieldId: field.id,
-                            value,
-                          });
-                        } else {
-                          createItem({
-                            entityId: entity.id,
-                            values: [{
-                              fieldId: field.id,
-                              value,
-                            }],
-                          });
-                        }
-                      }}
-                      style={{
-                        border: 'none',
-                        height: 25,
-                        overflowY: 'hidden',
-                        paddingLeft: 5,
-                        paddingRight: 50,
-                        paddingTop: 0,
-                        ...(field.type === 'TEXT' && {
-                          resize: 'vertical',
-                          width: '100%',
-                        }),
-                      }}
-                      useContentEditableDiv
-                    />
-                  );
-                } else if (field.type === 'BOOLEAN') {
-                  return (
-                    <Checkbox
-                      defaultIsChecked={defaultValue || false}
-                      onClick={async value => {
-                        updateFieldValueForItem({
-                          entityId: entity.id,
-                          itemId: row.id,
-                          fieldId: field.id,
-                          value,
-                        });
-                      }}
-                    />
-                  );
+      bordered
+      hover
+      responsive
+      style={{
+        overflowX: 'scroll',
+      }}
+    >
+      <thead>
+        <tr>
+          <th
+            style={{ width: 35 }}
+          >
+            <Checkbox
+              isChecked={allItemsAreChecked}
+              onClick={() => {
+                if (allItemsAreChecked) {
+                  onSelectAllItems([]);
+                } else {
+                  onSelectAllItems(items);
                 }
-              },
-              sortable: true,
-              type: 'cell',
-              value: row => row.values?.find(val => val.fieldId === field.id)?.value,
-            };
-          }),
+              }}
+            />
+          </th>
+          {
+            entity.fields
+              .map(field => (
+                <th key={field.id}>
+                  {field.name}
+                </th>
+              ))
+          }
+          <th style={{ width: 35 }} />
+        </tr>
+      </thead>
+      <tbody>
         {
-          type: 'options',
-        },
-      ]}
-      options={row => (
-        <Dropdown
-          style={{
-            position: 'static',
-          }}
-        >
-          <Dropdown.Toggle as={DropdownToggle} />
-          <Dropdown.Menu>
-            <Dropdown.Item
-              onClick={() => navigate(links.itemUpsert(entity.id, row.id))}
-            >
-              <FontAwesomeIcon
-                icon="edit"
-                style={{
-                  marginRight: 10,
-                  width: 25,
-                }}
+          (!items || !items.length)
+            && (
+              <ItemRow
+                entity={entity}
+                onAddItem={addItem}
+                onSelect={onItemSelect}
+                item={{}}
               />
-              {t('edit')}
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => createItem({
-                entityId: entity.id,
-                ...row,
-              })}
-            >
-              <FontAwesomeIcon
-                icon="copy"
-                style={{
-                  marginRight: 10,
-                  width: 25,
-                }}
+            )
+        }
+        {
+          items?.sort((t1, t2) => {
+              if (!t1.dueAt) return -1;
+              if (!t2.dueAt) return 1;
+              return t1.dueAt < t2.dueAt ? -1 : 1;
+            })
+            .map(item => (
+              <ItemRow
+                entity={entity}
+                isSelected={!!selectedItems.find(t1 => item.id === t1.id)}
+                key={`${item.id}-${item.dueAt}`}
+                onAddItem={addItem}
+                onSelect={onItemSelect}
+                item={item}
               />
-              {t('duplicate')}
-            </Dropdown.Item>
-            <Dropdown.Item
-              onClick={() => deleteItem({
-                entityId: entity.id,
-                id: row.id,
-              })}
-            >
-              <FontAwesomeIcon
-                icon="times"
-                style={{
-                  color: 'red',
-                  marginRight: 10,
-                  width: 25,
-                }}
-              />
-              {t('delete')}
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      )}
-      rows={rows}
-    />
+            ))
+        }
+      </tbody>
+    </Table>
   );
 };
 
--- src/components/organizations/OrganizationsList.tsx
+++ src/components/organizations/OrganizationsList.tsx
@@ -1,80 +1,70 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Table } from '@fikasio/react-ui-components';
-import DropdownToggle from 'components/UI/DropdownToggle';
-import { useGetOrganizationsQuery, useDeleteOrganizationMutation } from 'services/organizations/api';
-import links from 'utils/links';
-
-const OrganizationsList = () => {
+import Table from 'react-bootstrap/Table';
+import { Checkbox } from '@fikasio/react-ui-components';
+import OrganizationRow from './OrganizationRow';
+const OrganizationsList = ({
+  onOrganizationClick,
+  onOrganizationSelect,
+  onSelectAllOrganizations,
+  organizations,
+  selectedOrganizations,
+}) => {
   const { t } = useTranslation();
-  const { data: organizations } = useGetOrganizationsQuery();
-  const [deleteOrganization] = useDeleteOrganizationMutation();
 
-  return organizations ? (
+  const allOrganizationsAreChecked = !!organizations?.length
+    && (organizations?.length === selectedOrganizations.length);
+
+  return (
     <Table
-      columns={[
+      bordered
+      hover
+      responsive
+      style={{
+        overflowX: 'scroll',
+      }}
+    >
+      <thead>
+        <tr>
+          <th
+            style={{ width: 35 }}
+          >
+            <Checkbox
+              isChecked={allOrganizationsAreChecked}
+              onClick={() => {
+                if (allOrganizationsAreChecked) {
+                  onSelectAllOrganizations([]);
+                } else {
+                  onSelectAllOrganizations(organizations);
+                }
+              }}
+            />
+          </th>
+          <th style={{ minWidth: 500 }}>{t('name')}</th>
+          <th style={{ width: 35 }} />
+        </tr>
+      </thead>
+      <tbody>
         {
-          name: 'Name',
-          property: 'name',
-          render: row => (
-            <Link
-              style={{ textDecoration: 'none' }}
-              to={links.organization(row.id)}
-            >
-              <div
-                style={{
-                  color: '#000',
-                  height: '100%',
-                  width: '100%',
-                }}
-              >
-                { row.name }
-              </div>
-            </Link>
-          ),
-          type: 'cell',
-          value: row => row.name,
-        }, {
-          type: 'options',
-        }
-      ]}
-      options={row => (
-        <Dropdown
-          style={{
-            position: 'static',
-          }}
-        >
-          <Dropdown.Toggle as={DropdownToggle} />
-          <Dropdown.Menu>
-            <Dropdown.Item
-              onClick={() => deleteOrganization(row.id)}
-            >
-              <FontAwesomeIcon
-                icon="times"
-                style={{
-                  color: 'red',
-                  marginRight: 10,
-                  width: 25,
-                }}
+          organizations
+            .sort((o1, o2) => {
+              const p1NameNoAccents = o1.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+              const p2NameNoAccents = o2.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
+              return p1NameNoAccents < p2NameNoAccents ? -1 : 1;
+            })
+            .map(organization => (
+              <OrganizationRow
+                isSelected={!!selectedOrganizations.find(o1 => organization.id === o1.id)}
+                key={`${organization.id}-${organization.dueAt}`}
+                onClick={onOrganizationClick}
+                onSelect={onOrganizationSelect}
+                organization={organization}
               />
-              {t('delete')}
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      )}
-      rows={
-        organizations
-          .sort((o1, o2) => {
-            const p1NameNoAccents = o1.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-            const p2NameNoAccents = o2.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-            return p1NameNoAccents < p2NameNoAccents ? -1 : 1;
-          })
-      }
-    />
-  ) : <div />;
+            ))
+        }
+      </tbody>
+    </Table>
+  );
 };
 
 export default OrganizationsList;
