9f2560e724487020e9ed64594216660484b7cd0c
samdouble <samleafs@protonmail.com>
2024-12-23 00:02:26 -0500

    test: added Playwright config
    .
    
    .
    
    .
    
    .
    
    .

---
+++ playwright.config.ts
@@ -0,0 +1,69 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  expect: {
+    timeout: 5000,
+    toHaveScreenshot: {
+      maxDiffPixelRatio: 0.05,
+    },
+  },
+  forbidOnly: !!process.env.CI,
+  fullyParallel: true,
+  reporter: 'html',
+  retries: process.env.CI ? 2 : 0,
+  testDir: './tests',
+  testMatch: '**/*.visual.test.tsx',
+  timeout: 10 * 1000,
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    baseURL: 'http://localhost:3000',
+    screenshot: 'on',
+    trace: 'on-first-retry',
+    viewport: {
+      width: 1280,
+      height: 720
+    },
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    // TODO: Enable this once we have a way to run tests on Safari and Mobile Safari
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: {
+    //     ...devices['iPhone 12'],
+    //   },
+    // },
+  ],
+  webServer: {
+    command: 'yarn start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+});
