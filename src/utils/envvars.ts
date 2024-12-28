0c999eec81dbd4316e68ecb70ad7dc791a15fe93
samdouble <samleafs@protonmail.com>
2024-12-28 17:50:17 -0500

    fix: get environment variables from build step

--- src/utils/envvars.ts
+++ src/utils/envvars.ts
@@ -1,14 +1,19 @@
+export const getEnvVariable = (key: string): string => {
+  // @ts-ignore
+  return window._env_?.[key] ?? process.env[key] ?? '';
+};
+
 export const envvars = {
-  apiServer: process.env.REACT_APP_API_SERVER!,
-  googleAnalyticsId: process.env.REACT_APP_GOOGLE_ANALYTICS_ID!,
-  googleOAuthClientId: process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID!,
-  stripeConfirmationUrl: process.env.REACT_APP_STRIPE_CONFIRMATION_URL!,
-  stripePublishKey: process.env.REACT_APP_STRIPE_PUBLISH_KEY!,
-  urlDocumentation: process.env.REACT_APP_URL_DOCUMENTATION!,
-  urlPrivacy: process.env.REACT_APP_URL_PRIVACY!,
-  urlToS: process.env.REACT_APP_URL_TOS!,
-  urlWebsite: process.env.REACT_APP_URL_WEBSITE!,
-  websocketServer: process.env.REACT_APP_WEBSOCKET_SERVER!,
+  apiServer: getEnvVariable('REACT_APP_API_SERVER'),
+  googleAnalyticsId: getEnvVariable('REACT_APP_GOOGLE_ANALYTICS_ID'),
+  googleOAuthClientId: getEnvVariable('REACT_APP_GOOGLE_OAUTH_CLIENT_ID'),
+  stripeConfirmationUrl: getEnvVariable('REACT_APP_STRIPE_CONFIRMATION_URL'),
+  stripePublishKey: getEnvVariable('REACT_APP_STRIPE_PUBLISH_KEY'),
+  urlDocumentation: getEnvVariable('REACT_APP_URL_DOCUMENTATION'),
+  urlPrivacy: getEnvVariable('REACT_APP_URL_PRIVACY'),
+  urlToS: getEnvVariable('REACT_APP_URL_TOS'),
+  urlWebsite: getEnvVariable('REACT_APP_URL_WEBSITE'),
+  websocketServer: getEnvVariable('REACT_APP_WEBSOCKET_SERVER'),
 };
 
 export default envvars;
